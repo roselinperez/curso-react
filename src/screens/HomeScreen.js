09ef29ae967c8336c98851bab3a6437aea20d65f
unknown <roselin.redlaboral@gmail.com>
2019-09-04 16:04:29 -0500

    040920193

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,31 +5,36 @@ const HomeScreen = ({ navigation }) => {
 
   return (
   <View>
-    <Text style={styles.text}>Hi there!</Text>
+    <Text style={[styles.text, {color: 'red'}]}>HI THERE!</Text>
     <Button
         onPress = {() => navigation.navigate('Components')}
         title = "Go to Components Demo"
-
+        color = "gray"
     />
     <Button
         onPress = {() => navigation.navigate('List')}
         title = "Go to List Demo"
+        color = "orange"
     />
     <Button
         onPress = {() => navigation.navigate('Image')}
         title = "Go to Image Screen"
+        color = "blue"
     />
     <Button
         onPress = {() => navigation.navigate('Counter')}
         title = "Go to Counter Screen"
+        color = "purple"
     />
     <Button
         onPress = {() => navigation.navigate('Color')}
         title = "Go to Color Screen"
+        color = "red"
     />
     <Button
         onPress = {() => navigation.navigate('Square')}
         title = "Go to Square Color Screen"
+        color = "green"
     />
 
   </View>
